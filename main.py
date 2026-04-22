46832315c398193a545491ea92777f2d2d7d74ff
Felipe Duarte <felipeferreirafacul96@gmail.com>
2026-04-22 16:59:20 -0300

    ativando pipeline

--- main.py
+++ main.py
@@ -42,4 +42,4 @@ def cotacao():
     return jsonify(preco=preco[0])
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=5001)
